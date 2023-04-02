17fa8a3f7ee7dfb43a6f6638c4b89e9645f49e92
waldekk95 <waldekk95@gmail.com>
2023-04-02 20:49:31 +0200

    adding modal when user enter wrong data

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,26 @@
 import { useState } from "react";
 import "./Form.css";
 
+import ErrorModal from "./UI/ErrorModal";
+
 const Form = (props) => {
   const [userName, setUserName] = useState("");
   const [userAge, setUserAge] = useState("");
+  const [error, setError] = useState();
 
   const addItem = (event) => {
-    if (userName.trim().length === 0 || userAge < 0.01) {
+    if (userName.trim().length === 0) {
+      setError({
+        title: "Wprowadź nazwę",
+        message: "Nazwa nie może być pusta",
+      });
+      return;
+    }
+    if (userAge < 0.01) {
+      setError({
+        title: "Błędny wiek",
+        message: "Wprowadź prawidłowy wiek (większy od 0)",
+      });
       return;
     } else {
       event.preventDefault();
@@ -16,30 +30,43 @@ const Form = (props) => {
     }
   };
 
+  const errorHandler = () => {
+    setError(false);
+  };
+
   return (
-    <form className="form" onSubmit={addItem}>
-      <label className="form-input__name">Username</label>
-      <input
-        className="form-input"
-        type="text"
-        value={userName}
-        onChange={(evt) => {
-          setUserName(evt.target.value);
-        }}
-      ></input>
-      <label className="form-input__name">Age (years)</label>
-      <input
-        className="form-input"
-        type="number"
-        value={userAge}
-        onChange={(evt) => {
-          setUserAge(evt.target.value);
-        }}
-      ></input>
-      <button className="form-button" type="submit">
-        Add User
-      </button>
-    </form>
+    <>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        ></ErrorModal>
+      )}
+      <form className="form" onSubmit={addItem}>
+        <label className="form-input__name">Username</label>
+        <input
+          className="form-input"
+          type="text"
+          value={userName}
+          onChange={(evt) => {
+            setUserName(evt.target.value);
+          }}
+        ></input>
+        <label className="form-input__name">Age (years)</label>
+        <input
+          className="form-input"
+          type="number"
+          value={userAge}
+          onChange={(evt) => {
+            setUserAge(evt.target.value);
+          }}
+        ></input>
+        <button className="form-button" type="submit">
+          Add User
+        </button>
+      </form>
+    </>
   );
 };
 
--- src/components/UI/ErrorModal.jsx
+++ src/components/UI/ErrorModal.jsx
@@ -1,18 +1,23 @@
 import "./ErrorModal.css";
 
 const ErrorModal = (props) => {
+
+
   return (
-    <div className="modal">
-      <header className="modal-header">
-        <h2>{props.title}</h2>
-      </header>
-      <div>
-        <p>{props.message}</p>
+    <>
+    <div className="backdrop" onClick={props.onConfirm}></div>
+      <div className="modal">
+        <header className="modal-header">
+          <h2>{props.title}</h2>
+        </header>
+        <div className="modal-content">
+          <p>{props.message}</p>
+        </div>
+        <footer>
+          <button onClick={props.onConfirm}>Okej</button>
+        </footer>
       </div>
-      <footer>
-        <button>Okej</button>
-      </footer>
-    </div>
+    </>
   );
 };
 
