d6de2cd5f49ab458f0ab72ba9e4020576a25f57e
Waldekk95 <86876255+Waldekk95@users.noreply.github.com>
2024-11-26 14:42:04 +0100

    adding todo sorting by name

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -4,27 +4,34 @@ import React, { useState, useEffect } from "react";
 
 const List = (props: { items: any[] }) => {
   const [todos, setTodos] = useState([
-    { name: "Zrobic zakupy", time: 45, id: "A1" },
     { name: "Odkurzyć dom", time: 30, id: "A2" },
+    { name: "Zrobić todosa", time: 45, id: "A1" },
+    { name: "Napisać apkę", time: 20, id: "A3" },
   ]);
 
-  // Efekt, który aktualizuje stan przy dodaniu nowego elementu
   useEffect(() => {
     if (props.items.length > 0) {
-      // Dodajemy tylko ostatni dodany element z props.items
       const newItem = props.items[props.items.length - 1];
       setTodos((prevTodos) => [...prevTodos, newItem]);
     }
   }, [props.items]);
 
-  // Funkcja do usuwania elementu
+  const sortTodosByName = () => {
+    const sortTodos = [...todos];
+    sortTodos.sort((a, b) => a.name.localeCompare(b.name));
+    setTodos(sortTodos);
+  };
+
   const deleteItemHandler = (id: string) => {
     setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
   };
 
   return (
     <ul className="list">
-      <h3 className="list-title">List zadań</h3>
+      <h3 className="list-title">Lista zadań</h3>
+      <button className="form-button" onClick={() => sortTodosByName()} >
+        Sortuj po nazwie
+      </button>
       {todos.map((todo) => (
         <Item
           key={todo.id}
